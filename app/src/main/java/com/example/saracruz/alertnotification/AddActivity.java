813a3e577561f2e38380077071eec254888148e5
SARA <cruzgomezsara@gmail.com>
2019-01-08 22:50:33 +0100

    version 0.8 eliminado un toast que sobraba

--- app/src/main/java/com/example/saracruz/alertnotification/AddActivity.java
+++ app/src/main/java/com/example/saracruz/alertnotification/AddActivity.java
@@ -139,7 +139,7 @@ public class AddActivity extends AppCompatActivity {
     }
 
     private void startalarm(long timeInMillis) {
-        Toast.makeText(this, "He entrado", Toast.LENGTH_SHORT).show();
+        
         AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
         Intent intent = new Intent(this,AlarmReceiver.class);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
